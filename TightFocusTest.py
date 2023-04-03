71d576a44d9a022fc0d5bed1c52f6992f4c1b0e6
thariq <thariqshanavas@gmail.com>
2023-04-03 11:15:19 -0600

    modified:   TightFocusTest.py

--- TightFocusTest.py
+++ TightFocusTest.py
@@ -11,18 +11,17 @@ start_time = time.time()
 # Simulation parameters
 wavelength = 500e-9
 dz = 50e-9
-dx = dy = 2*wavelength # Minimum resolution = lambda/(n*sqrt(2)) for finite difference. Any lower and the algorithm is numerically unstable
 n_h = 1  # Homogenous part of refractive index
 xy_cells = 1024    # Keep this a power of 2 for efficient FFT
 
-beam_radius = 15e-6
-focus_depth = 30e-6
-
+beam_radius = 1e-3
+focus_depth = 2e-3
+dx = dy = 10*beam_radius/(xy_cells)
 if 2*beam_radius > 0.5*dx*xy_cells:
     # Beam diameter greater than half the length of the simulation cross section.
     ValueError("Beam is larger than simulation cross section")
 
-beam_type = 'G' # 'HG, 'LG', 'G'
+beam_type = 'LG' # 'HG, 'LG', 'G'
 l = 1  # Topological charge for LG beam
 (u,v) = (1,0)   # Mode numbers for HG beam
 
@@ -33,7 +32,7 @@ elif beam_type=='HG':
 else:
     seed = Gaussian_beam(xy_cells, dx, beam_radius)
 
-Ex,Ey,Ez,dx_TightFocus = TightFocus(seed,dx,wavelength,n_h,focus_depth,0,0.1)
+Ex,Ey,Ez,dx_TightFocus = TightFocus(seed,dx,wavelength,n_h,focus_depth,6e-6,0.1)
 
 
 indices = np.linspace(-xy_cells/2,xy_cells/2-1,xy_cells,dtype=np.int_)
