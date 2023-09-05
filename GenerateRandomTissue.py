8f9b1de7d4ff52bed9ee9e451052dab8f7a45f27
Thariq <thariqshanavas@gmail.com>
2023-09-04 23:28:31 -0600

    critical bugfix on tissue model

--- GenerateRandomTissue.py
+++ GenerateRandomTissue.py
@@ -22,14 +22,15 @@ def RandomTissue(args):
     sigma_x = np.interp(g,g_vs_sigma_x[:,1],g_vs_sigma_x[:,0])*wavelength
     if sigma_x < 1.5*dx:
         print('Warning: fluctuations in tissue index finer than transverse resolution')
-    n_ih = np.zeros((xy_cells,xy_cells,unique_layers),dtype=np.float32)
+    d_phi = np.zeros((xy_cells,xy_cells,unique_layers),dtype=np.float32)
     indices = np.linspace(-xy_cells/2,xy_cells/2-1,xy_cells,dtype=np.int_)
     xx, yy = np.meshgrid(dx*indices,dx*indices)
     mask = np.exp(-(xx**2+yy**2)/(2*sigma_x**2))
 
     for i in range(0,unique_layers):
-        n_ih[:,:,i] = rand.normal(scale = sigma_p, size=(xy_cells,xy_cells))
-        n_ih[:,:,i] = signal.fftconvolve(n_ih[:,:,i],mask,mode='same')
+        d_phi[:,:,i] = rand.normal(scale = sigma_p, size=(xy_cells,xy_cells))
+        d_phi[:,:,i] = signal.fftconvolve(d_phi[:,:,i],mask,mode='same')
 
-    n_ih = n_ih*sigma_p/norm.fit(n_ih.flatten())[1]  # Normalizing to make sure phase profile has a std. deviation of sigma_p after the convolution
+    d_phi = d_phi*sigma_p/norm.fit(d_phi.flatten())[1]  # Normalizing to make sure phase profile has a std. deviation of sigma_p after the convolution
+    n_ih = d_phi*wavelength/(dz*2*np.pi) # Convert phase mask into refractive index fluctuation
     return n_h+n_ih
